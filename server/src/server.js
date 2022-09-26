2c68f3c4bc399b5c311714c6d185a37dbbb6c745
Rakenek <kamilotawski@gmail.com>
2022-09-26 15:32:15 +0200

    160

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
+const { MONGO_URL } = require("../env");
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
@@ -8,7 +10,16 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
