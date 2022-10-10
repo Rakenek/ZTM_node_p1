f10d7059b417191fe0af5fc9d9ced0eabd10ed14
Rakenek <kamilotawski@gmail.com>
2022-10-10 16:00:04 +0200

    182

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -20,8 +20,8 @@ saveLaunch(launch);
 
 // launches.set(launch.flightNumber, launch);
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({ flightNumber: launchId });
 }
 
 async function getLatestFlightNumber() {
@@ -51,7 +51,7 @@ async function saveLaunch(launch) {
     throw new Error("No matching planet found");
   }
 
-  await launchesDatabase.updateOne(
+  await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
     },
@@ -86,11 +86,23 @@ async function scheduleNewLaunch(launch) {
 //   );
 // }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId) {
+  const aborted = await launchesDatabase.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+  console.log(aborted);
+  return aborted.acknowledged === true && aborted.modifiedCount === 1;
+
+  // const aborted = launches.get(launchId);
+  // aborted.upcoming = false;
+  // aborted.success = false;
+  // return aborted;
 }
 
 module.exports = {
