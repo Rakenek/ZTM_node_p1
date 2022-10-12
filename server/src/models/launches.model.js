b661ed702b0174624e1bf7effcdbd406506303ed
Rakenek <kamilotawski@gmail.com>
2022-10-12 13:28:02 +0200

    193

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const launchesDatabase = require("./launches.mongo");
 const planets = require("./planets.mongo");
 const DEFAULT_FLIGHT_NUMBER = 100;
@@ -17,6 +19,52 @@ const launch = {
 };
 
 saveLaunch(launch);
+
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
+
+async function loadLaunchesData() {
+  console.log("Downloading launch data...");
+  const response = await axios.post(SPACEX_API_URL, {
+    query: {},
+    options: {
+      populate: [
+        {
+          path: "rocket",
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: "payloads",
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+  });
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc["payloads"];
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers,
+    };
+
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
+}
 
 // launches.set(launch.flightNumber, launch);
 
@@ -110,4 +158,5 @@ module.exports = {
   getAllLaunches,
   scheduleNewLaunch,
   abortLaunchById,
+  loadLaunchesData,
 };
